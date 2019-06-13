a9fe206c7004380bceb103f0d2b922e7e94333b1
Harrison Cole <harrison.d.cole@gmail.com>
2019-06-13 17:27:30 -0400

    even out hive cell positions

--- src/components/HiveCell.js
+++ src/components/HiveCell.js
@@ -10,7 +10,7 @@ const HiveCell = ({ cellLocation, letter }) => {
       <polygon
         points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263"
         stroke="white"
-        stroke-width="7.5"
+        stroke-width="5"
       />
       <text className="letter" x="50%" y="50%" dy="10.75%">
         {letter}
