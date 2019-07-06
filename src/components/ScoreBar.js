2c9a65ab8416c57b47fd19eb50fe04930a2bce84
Harrison Cole <harrison.d.cole@gmail.com>
2019-07-05 22:03:34 -0700

    progress bar styling

--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -3,9 +3,24 @@ import { TotalScore } from './index';
 import './ScoreBar.css';
 
 const ScoreBar = ({ score }) => {
+  const levels = [
+    'good-start',
+    'moving-up',
+    'good',
+    'solid',
+    'Nice',
+    'Great',
+    'Amazing',
+    'Genius',
+  ];
   return (
     <div className="score-bar">
       <TotalScore score={score} />
+      <div className="progress-dots">
+        {levels.map(level => (
+          <div key={level} className={`${level} dot`} />
+        ))}
+      </div>
     </div>
   );
 };
